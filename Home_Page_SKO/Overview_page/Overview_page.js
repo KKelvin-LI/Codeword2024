55ecd40fdfccb81c4bd47139da95a5cf1c830688
KKelvin-LI <s4004448@student.rmit.edu.au>
2024-10-03 01:54:49 +1000

    Add files via upload

---
+++ Home_Page_SKO/Overview_page/Overview_page.js
@@ -0,0 +1,16 @@
+function preload(){
+  img1 =loadImage("images/god.png")
+  img2 =loadImage("images/PythSoc.png")
+}
+
+function setup() {
+createCanvas(windowWidth,windowHeight);
+bacground(0);
+
+}
+
+
+function draw() {
+  background(0);
+  
+}
